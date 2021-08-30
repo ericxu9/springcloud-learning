757bc8ca05f058402c8b51100920a9235b47d67b
xumiao <xuyongjun96@gmail.com>
2021-08-30 22:32:48 +0800

    init

---
+++ config-server/src/test/java/org/xumiao/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xumiao.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
